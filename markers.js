3c3cf5cb53d01fb8bd8537a4aa71aa5c7f48cc95
Shanfer Majeed (Student) <mmajeed2@sundevils.asu.edu>
2023-02-02 12:00:11 -0700

    tutorial

---
+++ markers.js
@@ -0,0 +1,25 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+var greenIcon = new LeafIcon({iconUrl: 'https://icons8.com/icon/31715/oak-leaf'}),
+    redIcon = new LeafIcon({iconUrl: 'icons8-oak-leaf-16.png'}),
+    orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+L.marker([51.495, -0.083], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
+L.marker([51.49, -0.1], {icon: orangeIcon}).addTo(map).bindPopup("I am an orange leaf.");
--- myscripts.js
+++ myscripts.js
@@ -11,13 +11,33 @@ const basemaps = {
     iconSize: [40, 40],
   });
 
-  const marker1 = L.marker([-37.699450, 176.279420], {icon: basicBeachIcon}).addTo(map);
-const marker2 = L.marker([-27.643310, 153.305140], {icon: basicBeachIcon}).addTo(map);
-const marker3 = L.marker([-33.956330, 122.150270], {icon: basicBeachIcon}).addTo(map);
-const marker4 = L.marker([-34.962390, 117.391220], {icon: basicBeachIcon}).addTo(map);
-const marker5 = L.marker([-17.961210, 122.214820], {icon: basicBeachIcon}).addTo(map);
-const marker6 = L.marker([-16.505960, 151.751520], {icon: basicBeachIcon}).addTo(map);
-const marker7 = L.marker([-22.594400, 167.484440], {icon: basicBeachIcon}).addTo(map);
-const marker8 = L.marker([-37.977000, 177.057000], {icon: basicBeachIcon}).addTo(map);
-const marker9 = L.marker([-41.037600, 173.017000], {icon: basicBeachIcon}).addTo(map);
-const marker10 = L.marker([-37.670300, 176.212000], {icon: basicBeachIcon}).addTo(map);
+  const marker1 = L.marker([-37.699450, 176.279420], {icon: basicBeachIcon})
+  .bindPopup('Whitehaven Beach, Whitsunday Island')
+  .addTo(map);
+const marker2 = L.marker([-27.643310, 153.305140], {icon: basicBeachIcon})
+  .bindPopup('Turquoise Bay Exmouth, Australia')
+  .addTo(map);
+const marker3 = L.marker([-33.956330, 122.150270], {icon: basicBeachIcon})
+  .bindPopup('Cape Le Grand National Park Esperance, Australia')
+  .addTo(map);
+const marker4 = L.marker([-34.962390, 117.391220], {icon: basicBeachIcon})
+  .bindPopup('Greens Pool Denmark, Australia')
+  .addTo(map);
+const marker5 = L.marker([-17.961210, 122.214820], {icon: basicBeachIcon})
+  .bindPopup('Cable Beach Broome, Australia')
+  .addTo(map);
+const marker6 = L.marker([-16.505960, 151.751520], {icon: basicBeachIcon})
+  .bindPopup('Matira Beach, Society Islands')
+  .addTo(map);
+const marker7 = L.marker([-22.594400, 167.484440], {icon: basicBeachIcon})
+  .bindPopup('Piscine Naturelle Ile Des Pins, New Caledonia')
+  .addTo(map);
+const marker8 = L.marker([-37.977000, 177.057000], {icon: basicBeachIcon})
+  .bindPopup('Ohope Beach Whakatane, New Zealand')
+  .addTo(map);
+const marker9 = L.marker([-41.037600, 173.017000], {icon: basicBeachIcon})
+  .bindPopup('Kaiteriteri Beach, New Zealand')
+  .addTo(map);
+const marker10 = L.marker([-37.670300, 176.212000], {icon: basicBeachIcon})
+  .bindPopup('Mt Maunganui Main Beach, New Zealand')
+  .addTo(map);
